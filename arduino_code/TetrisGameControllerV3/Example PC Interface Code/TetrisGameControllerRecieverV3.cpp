65ad752e97bf10c8780708209a87a42b3087c425
sudo-Eric <ericheinke0@gmail.com>
2022-04-15 16:16:05 -0700

    Added Arduino code

---
+++ arduino_code/TetrisGameControllerV3/Example PC Interface Code/TetrisGameControllerRecieverV3.cpp
@@ -0,0 +1,214 @@
+/**
+ * @file /SerialCommunications_Testing.cpp
+ * @author Eric Heinke
+ * @date February 7 2022
+ * @brief File containing a test of serial port communication
+ */
+
+#include <iostream>
+#include <bitset>
+#include <string>
+
+// Serial library
+#include "lib/serialib.h"
+// Serial library status code decoding
+#include "DecodeStatusCodes.h"
+
+#include <chrono>
+using namespace std::chrono;
+
+#if defined (_WIN32) || defined(_WIN64)
+    #include<windows.h>
+#endif
+#if defined (__linux__) || defined(__APPLE__)
+    #include<unistd.h>
+#endif
+
+#if defined (_WIN32) || defined(_WIN64)
+    //for serial ports above "COM9", we must use this extended syntax of "\\.\COMx".
+    //also works for COM0 to COM9.
+    #define SERIAL_PORT "\\\\.\\COM4"
+#endif
+#if defined (__linux__) || defined(__APPLE__)
+    #define SERIAL_PORT "/dev/ttyACM0"
+#endif
+
+using namespace std;
+
+void decodeInputs(char* bytes)
+{
+    // for (int i = 0; i < 14; ++i)
+    // {
+    //     cout<<"byte"<<to_string(i)<<": "<<bitset<8>(bytes[i]).to_string()<<endl;
+    // }
+
+    char buttons = ~(((bytes[2] & 0b00000011) << 6) | ((bytes[5] & 0b00000011) << 4) | 
+                    ((bytes[8] & 0b00000011) << 2) | (bytes[11] & 0b00000011));
+    int joystick1X = ((bytes[0] & 0b00111111) << 2) | ((bytes[1] & 0b00110000) >> 4);
+    int joystick1Y = ((bytes[1] & 0b00001111) << 4) | ((bytes[2] & 0b00111100) >> 2);
+    bool joystickB1 = !((bytes[13] & 0b00001000) >> 3);
+    int joystick2X = ((bytes[3] & 0b00111111) << 2) | ((bytes[4] & 0b00110000) >> 4);
+    int joystick2Y = ((bytes[4] & 0b00001111) << 4) | ((bytes[5] & 0b00111100) >> 2);
+    bool joystickB2 = !((bytes[13] & 0b00000100) >> 2);
+    int joystickSX = ((bytes[6] & 0b00111111) << 2) | ((bytes[7] & 0b00110000) >> 4);
+    int joystickSY = ((bytes[7] & 0b00001111) << 4) | ((bytes[8] & 0b00111100) >> 2);
+    bool buttonC = ((bytes[13] & 0b00000010) >> 1);
+    bool buttonZ = (bytes[13] & 0b00000001);
+    int accelerometerAX = ((bytes[9] & 0b00111111) << 2) | ((bytes[10] & 0b00110000) >> 4);
+    int accelerometerAY = ((bytes[10] & 0b00001111) << 4) | ((bytes[11] & 0b00111100) >> 2);
+    int accelerometerAZ = ((bytes[12] & 0b00111111) << 2) | ((bytes[13] & 0b00110000) >> 4);
+    int accelerometerAP = ((bytes[14] & 0b00111111) << 2) | ((bytes[15] & 0b00110000) >> 4);
+    int accelerometerAR = ((bytes[15] & 0b00001111) << 4) | ((bytes[16] & 0b00111100) >> 2);
+    bool unused1 = ((bytes[16] & 0b00000010) >> 1);
+    bool unused2 = (bytes[16] & 0b00000001);
+
+    cout<<"Buttons: "<<bitset<8>(buttons).to_string()<<endl;
+    cout<<"Joystick 1: ("<<to_string(joystick1X)<<", "<<to_string(joystick1Y)<<") Button: "<<to_string(joystickB1)<<endl;
+    cout<<"Joystick 2: ("<<to_string(joystick2X)<<", "<<to_string(joystick2Y)<<") Button: "<<to_string(joystickB2)<<endl;
+    cout<<"Joystick N: ("<<to_string(joystickSX)<<", "<<to_string(joystickSY)<<")";
+    cout<<" Button C: "<<to_string(buttonC)<<" Button Z: "<<to_string(buttonZ)<<endl;
+    cout<<"Accelerometer A: ("<<to_string(accelerometerAX)<<", "<<to_string(accelerometerAY)<<", "<<to_string(accelerometerAZ)<<")"<<endl;
+    cout<<"Rotations (P,R): ("<<to_string(accelerometerAP)<<", "<<to_string(accelerometerAR)<<")"<<endl;
+    // cout<<bytes<<endl;
+}
+
+int main(int argc, char *argv[])
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Serial Communication
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Serial object
+    serialib serial;
+    DecodeStatusCodes statusCodes(false);
+
+    // Connection to serial port
+    char statusCode = serial.openDevice(SERIAL_PORT, 57600, SERIAL_DATABITS_8, SERIAL_PARITY_NONE, SERIAL_STOPBITS_1);
+
+    // If connection fails, display error message and exit. Otherwise, display a success message
+    if (statusCodes.openDevice(statusCode) == false) return 1;
+
+    statusCode = serial.flushReceiver();
+
+    // Display status code message
+    statusCodes.flushReceiver(statusCode);
+
+    cout<<"Select operation to perform:"<<endl;
+    cout<<"\t1: Get controller status (x1)"<<endl;
+    cout<<"\t2: Get controller status (x10)"<<endl;
+    cout<<"\t3: Get controller status (infinite)"<<endl;
+    cout<<"\t4: Play tower light animation"<<endl;
+    cout<<"\t5: Play flatline sound effect"<<endl;
+    cout<<"\t6: Nunchuck Controller Settings"<<endl;
+    cout<<"\t7: Set button color"<<endl;
+    cout<<"> ";
+    int mode;
+    cin>>mode;
+
+    if (mode == 1)
+    {
+        serial.writeString("CT\n");
+        char* bytes = new char[100];
+        statusCode = serial.readString(bytes, '\n', 100, 16);
+        statusCodes.readString(statusCode);
+        if (statusCode > 0)
+            decodeInputs(bytes);
+        delete bytes;
+    }
+    else if (mode == 2)
+    {
+        for (int i=0; i<10; i++)
+        {
+            serial.writeString("CT\n");
+            char* bytes = new char[100];
+            statusCode = serial.readString(bytes, '\n', 100, 16);
+            statusCodes.readString(statusCode);
+            if (statusCode > 0)
+                decodeInputs(bytes);
+            delete bytes;
+            Sleep(500);
+        }
+    }
+    else if (mode == 3)
+    {
+        while (true)
+        {
+            serial.writeString("CT\n");
+            char* bytes = new char[100];
+            statusCode = serial.readString(bytes, '\n', 100, 16);
+            statusCodes.readString(statusCode);
+            if (statusCode > 0)
+                decodeInputs(bytes);
+            delete bytes;
+            Sleep(500);
+        }
+    }
+    else if (mode == 4)
+    {
+        // Tower light animation
+        serial.writeString("AN1\n"); // Finish later
+    }
+    else if (mode == 5)
+    {
+        // Flatline sound
+        serial.writeString("AN2\n"); // Finish later
+    }
+    else if (mode == 6)
+    {
+        cout<<"Select Nunchuck operation to perform:"<<endl;
+        cout<<"\t1: Enable Nunchuck"<<endl;
+        cout<<"\t2: Disable Nunchuck"<<endl;
+        cout<<"\t3: Reset Nunchuck"<<endl;
+        cout<<"\t4: Toggle Nunchuck"<<endl;
+        cout<<"> ";
+        mode;
+        cin>>mode;
+        if (mode == 1)
+        {
+            serial.writeString("CFNCE\n");
+        }
+        else if (mode == 2)
+        {
+            serial.writeString("CFNCD\n");
+        }
+        else if (mode == 3)
+        {
+            serial.writeString("CFNCR\n");
+        }
+        else if (mode == 4)
+        {
+            serial.writeString("CFNCT\n");
+        }
+    }
+    else if (mode == 7)
+    {
+        char R[3];
+        char G[3];
+        char B[3];
+        cout<<"R: ";
+        cin.ignore();
+        cin.getline(R, 3);
+        cout<<"G: ";
+        cin.getline(G, 3);
+        cout<<"B: ";
+        cin.getline(B, 3);
+        cout<<"Setting button color to ("<<R<<", "<<G<<", "<<B<<")"<<endl;
+        char command[10] = {'U', 'L', 'K', R[0], R[1], G[0], G[1], B[0], B[1], '\n'};
+        serial.writeString(command);
+        
+        // while (!serial.available()) { }
+        // char* bytes = new char[100];
+        // statusCode = serial.readString(bytes, '\n', 100, 16);
+        // statusCodes.readString(statusCode);
+        // cout<<bytes;
+        // delete bytes;
+    }
+    else
+    {
+        cout<<"You have entered an invalid mode."<<endl;
+    }
+
+    // Close the serial device
+    serial.closeDevice();
+
+    return 0 ;
+}
---
+++ arduino_code/TetrisGameControllerV3/TetrisGameControllerV3.ino
@@ -0,0 +1,408 @@
+#include <DIO2.h>
+#include <WiiChuck.h>
+#include <Adafruit_NeoPixel.h>
+
+// This program requires the D2IO, WiiChuck, and Adafruit NeoPixel libraries. Make sure to install them.
+
+// For information on Wii nunchuck use
+// https://learn.adafruit.com/adafruit-wii-nunchuck-breakout-adapter/arduino-use
+
+// Serial variables
+boolean echoEnabled = false;
+
+// Status for various LEDs
+byte towerLEDState = 0;
+long buttonColor = 0xFFFFFF;
+
+// Nunchuck controller variables
+Accessory nunchuck1;
+boolean nunchuckEnabled;
+int joystick1X;
+int joystick1Y;
+int joystick2X;
+int joystick2Y;
+int joystickSX;
+int joystickSY;
+int accelerometerAX;
+int accelerometerAY;
+int accelerometerAZ;
+int accelerometerAP;
+int accelerometerAR;
+boolean buttonC;
+boolean buttonZ;
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel buttonPixels(8, 32, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // Builtin LED (L)
+  pinMode2(LED_BUILTIN, OUTPUT);
+  
+  // Setting up button input pins
+  pinMode2f(DP22, INPUT_PULLUP); // Up (instant fall)
+  pinMode2f(DP23, INPUT_PULLUP); // Down (fast fall)
+  pinMode2f(DP24, INPUT_PULLUP); // Left
+  pinMode2f(DP25, INPUT_PULLUP); // Right
+  pinMode2f(DP26, INPUT_PULLUP); // A (rotate clockwise)
+  pinMode2f(DP27, INPUT_PULLUP); // B (rotate counterclockwise)
+  pinMode2f(DP28, INPUT_PULLUP); // Select (hold piece)
+  pinMode2f(DP29, INPUT_PULLUP); // Start (start/pause)
+  
+  // Setting up joystick and joystick button input pins
+  pinMode(A0, INPUT);    // Joystick 1 X
+  pinMode(A1, INPUT);    // Joystick 1 Y
+  pinMode2f(DP30, INPUT_PULLUP); // Joystick 1 button
+  pinMode(A2, INPUT);    // Joystick 2 X
+  pinMode(A3, INPUT);    // Joystick 2 Y
+  pinMode2f(DP31, INPUT_PULLUP); // Joystick 2 button
+
+  // Setting up tower LED output pins
+  pinMode2f(DP46, OUTPUT); // Tower 1 Green
+  pinMode2f(DP47, OUTPUT); // Tower 1 Yellow
+  pinMode2f(DP48, OUTPUT); // Tower 1 Red
+  pinMode2f(DP49, OUTPUT); // Tower 1 Buzzer
+  pinMode2f(DP50, OUTPUT); // Tower 2 Green
+  pinMode2f(DP51, OUTPUT); // Tower 2 Yellow
+  pinMode2f(DP52, OUTPUT); // Tower 2 Red
+  pinMode2f(DP53, OUTPUT); // Tower 2 Buzzer
+  
+  // start serial port at 115200 bps, 8 bits per transfer, no parity, one stop bit:
+  Serial.begin(57600, SERIAL_8N1);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Initialize nunchuck values
+  nunchuckEnabled = false;
+
+  buttonPixels.begin();
+  buttonPixels.show();
+  buttonPixels.clear();
+  
+  // Tower lights startup animation
+  if (false)
+  {
+    towerLightAnimation();
+  }
+}
+
+void towerLightAnimation()
+{
+  digitalWrite2f(DP46, LOW);
+  digitalWrite2f(DP50, LOW);
+  digitalWrite2f(DP47, LOW);
+  digitalWrite2f(DP51, LOW);
+  digitalWrite2f(DP48, LOW);
+  digitalWrite2f(DP52, LOW);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+  delay(400);
+  digitalWrite2f(DP46, HIGH);
+  digitalWrite2f(DP50, HIGH);
+  delay(400);
+  digitalWrite2f(DP46, LOW);
+  digitalWrite2f(DP50, LOW);
+  digitalWrite2f(DP47, HIGH);
+  digitalWrite2f(DP51, HIGH);
+  delay(400);
+  digitalWrite2f(DP47, LOW);
+  digitalWrite2f(DP51, LOW);
+  digitalWrite2f(DP48, HIGH);
+  digitalWrite2f(DP52, HIGH);
+  delay(400);
+  digitalWrite2f(DP48, LOW);
+  digitalWrite2f(DP52, LOW);
+  digitalWrite2f(DP49, HIGH);
+  digitalWrite2f(DP53, HIGH);
+  delay(200);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+}
+
+void flatline()
+{
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+  delay(200);
+  digitalWrite2f(DP49, HIGH);
+  digitalWrite2f(DP53, HIGH);
+  delay(300);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+  delay(700);
+  digitalWrite2f(DP49, HIGH);
+  digitalWrite2f(DP53, HIGH);
+  delay(300);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+  delay(700);
+  digitalWrite2f(DP49, HIGH);
+  digitalWrite2f(DP53, HIGH);
+  delay(300);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+  delay(700);
+  digitalWrite2f(DP49, HIGH);
+  digitalWrite2f(DP53, HIGH);
+  delay(2000);
+  digitalWrite2f(DP49, LOW);
+  digitalWrite2f(DP53, LOW);
+}
+
+void sendControllerStatus()
+{
+  // Get analog readings
+  joystick1X = map(analogRead(A0),0,1023,0,255);
+  joystick1Y = map(analogRead(A1),0,1023,0,255);
+  joystick2X = map(analogRead(A2),0,1023,0,255);
+  joystick2Y = map(analogRead(A3),0,1023,0,255);
+
+  if (nunchuckEnabled)
+  {
+    nunchuck1.readData(); // Read inputs and update maps for nunchuck
+    joystickSX = nunchuck1.getJoyX();
+    joystickSY = nunchuck1.getJoyY();
+    accelerometerAX = nunchuck1.getAccelX();
+    accelerometerAY = nunchuck1.getAccelY();
+    accelerometerAZ = nunchuck1.getAccelZ();
+    accelerometerAP = nunchuck1.getPitchAngle();
+    accelerometerAR = nunchuck1.getRollAngle();
+    buttonC = nunchuck1.getButtonC();
+    buttonZ = nunchuck1.getButtonZ();
+  }
+  else
+  {
+    joystick1X = 0;;
+    joystick1Y = 0;
+    joystick2X = 0;
+    joystick2Y = 0;
+    joystickSX = 0;
+    joystickSY = 0;
+    accelerometerAX = 0;
+    accelerometerAY = 0;
+    accelerometerAZ = 0;
+    accelerometerAP = 0;
+    accelerometerAR = 0;
+    buttonC = false;
+    buttonZ = false;
+  }
+  
+  //                          Joystick 1X (7-2)
+  byte byte0 = 0b01000000 | ((joystick1X & 0b11111100) >> 2);
+  
+  //                           Joystick 1X (1-0)                  Joystick 1Y (7-4)
+  byte byte1 = 0b01000000 | (((joystick1X & 0b00000011) << 4) | ((joystick1Y & 0b11110000) >> 4));
+
+  //                          Joystick 1Y (3-0)                  B1 (instant fall)           B2 (fast fall)
+  byte byte2 = 0b01000000 | (((joystick1Y & 0b00001111) << 2) | (digitalRead2f(DP22) << 1) | digitalRead2f(DP23));
+
+  //                          Joystick 2X (7-2)
+  byte byte3 = 0b01000000 | ((joystick2X & 0b11111100) >> 2);
+
+  //                           Joystick 2X (1-0)                  Joystick 2Y (7-4)
+  byte byte4 = 0b01000000 | (((joystick2X & 0b00000011) << 4) | ((joystick2Y & 0b11110000) >> 4));
+
+  //                           Joystick 2Y (3-0)                 B3 (left)                   B4 (right)
+  byte byte5 = 0b01000000 | (((joystick2Y & 0b00001111) << 2) | (digitalRead2f(DP24) << 1) | digitalRead2f(DP25));
+
+  //                          Joystick SX (7-2)
+  byte byte6 = 0b01000000 | ((joystickSX & 0b11111100) >> 2);
+
+  //                           Joystick SX (1-0)                  Joystick SY (7-4)
+  byte byte7 = 0b01000000 | (((joystickSX & 0b00000011) << 4) | ((joystickSY & 0b11110000) >> 4));
+
+  //                           Joystick SY (3-0)                 B5 (A)                      B6 (B)
+  byte byte8 = 0b01000000 | (((joystickSY & 0b00001111) << 2) | (digitalRead2f(DP26) << 1) | digitalRead2f(DP27));
+
+  //                          Accelerometer AX (7-2)
+  byte byte9 = 0b01000000 | ((accelerometerAX & 0b11111100) >> 2);
+
+  //                            Accelerometer AX (1-0)                  Accelerometer AY (7-4)
+  byte byte10 = 0b01000000 | (((accelerometerAX & 0b00000011) << 4) | ((accelerometerAY & 0b11110000) >> 4));
+
+  //                            Accelerometer AY (3-0)                 B7 (Select)                 B8 (Start)
+  byte byte11 = 0b01000000 | (((accelerometerAY & 0b00001111) << 2) | (digitalRead2f(DP28) << 1) | digitalRead2f(DP29));
+
+  //                          Accelerometer AZ (7-2)
+  byte byte12 = 0b01000000 | ((accelerometerAZ & 0b11111100) >> 2);
+
+  //                            Accelerometer AZ (1-0)                 J1                           J2                           C               Z
+  byte byte13 = 0b01000000 | (((accelerometerAZ & 0b00000011) << 4) | (digitalRead2f(DP30) << 3) | (digitalRead2f(DP31) << 2) | (buttonC << 1) | buttonZ);
+
+  //                           Accelerometer AP (7-2)
+  byte byte14 = 0b01000000 | ((accelerometerAP & 0b11111100) >> 2);
+
+  //                            Accelerometer AP (1-0)                  Accelerometer AR (7-4)
+  byte byte15 = 0b01000000 | (((accelerometerAP & 0b00000011) << 4) | ((accelerometerAR & 0b11110000) >> 4));
+
+  //                            Accelerometer AR (3-0)                 Unused (1-0)
+  byte byte16 = 0b01000000 | (((accelerometerAR & 0b00001111) << 2) | (false << 1) | false);
+
+  Serial.write(byte0);
+  Serial.write(byte1);
+  Serial.write(byte2);
+  Serial.write(byte3);
+  Serial.write(byte4);
+  Serial.write(byte5);
+  Serial.write(byte6);
+  Serial.write(byte7);
+  Serial.write(byte8);
+  Serial.write(byte9);
+  Serial.write(byte10);
+  Serial.write(byte11);
+  Serial.write(byte12);
+  Serial.write(byte13);
+  Serial.write(byte14);
+  Serial.write(byte15);
+  Serial.write(byte16);
+  Serial.write("\n");
+}
+
+void setTowerLEDs(byte state)
+{
+  towerLEDState = state;
+  digitalWrite2f(DP46, state & 0b10000000); // Tower 1 Green
+  digitalWrite2f(DP47, state & 0b01000000); // Tower 1 Yellow
+  digitalWrite2f(DP48, state & 0b00100000); // Tower 1 Red
+  digitalWrite2f(DP49, state & 0b00010000); // Tower 1 Buzzer
+  digitalWrite2f(DP50, state & 0b00001000); // Tower 2 Green
+  digitalWrite2f(DP51, state & 0b00000100); // Tower 2 Yellow
+  digitalWrite2f(DP52, state & 0b00000010); // Tower 2 Red
+  digitalWrite2f(DP53, state & 0b00000001); // Tower 2 Buzzer
+}
+
+void loop() {
+    if (Serial.available() > 0)
+    {
+      char* buffer = new char[10];
+      int count = Serial.readBytesUntil('\n', buffer, 10);
+      int marker = 0;
+
+      // Serial Echo Enable/Disable
+      if (buffer[0] == 'E')
+      {
+        if (buffer[1] == 'E')
+          echoEnabled = true;
+        else if (buffer[1] == 'D')
+          echoEnabled = false;
+      }
+
+      if (echoEnabled)
+      {
+        Serial.write(buffer, count);
+        Serial.println();
+      }
+
+      // Arduino Status
+      if (buffer[0] == 'S' && buffer[1] == 'T')
+      {
+        // Print status to serial port
+        Serial.println("Arduino Game Controller");
+        Serial.println(" * Version: V3");
+        Serial.println(" * Optimized for: Tetris");
+        Serial.print(" * System status: ");
+        if (echoEnabled) {Serial.print("EE ");} else {Serial.print("ED ");}
+        if (nunchuckEnabled) {Serial.print("NE ");} else {Serial.print("ND ");}
+        Serial.println();
+      }
+
+      // Controller Status
+      if (buffer[0] == 'C' && buffer[1] == 'T')
+        sendControllerStatus();
+
+      // Configure
+      if (buffer[0] == 'C' && buffer[1] == 'F')
+      {
+        // Nunchuck
+        if (buffer[2] == 'N' && buffer[3] == 'C')
+        {
+          if (buffer[4] == 'E')
+            nunchuckEnabled = true;
+          if (buffer[4] == 'D')
+            nunchuckEnabled = false;
+          if (buffer[4] == 'T')
+            nunchuckEnabled = !nunchuckEnabled;
+          if (buffer[4] == 'R' && nunchuckEnabled)
+            nunchuck1.reset();
+          if (nunchuckEnabled && buffer[4] != 'R')
+          {
+            //Start nunchuck driver
+            nunchuck1.begin();
+            if (nunchuck1.type == Unknown) nunchuck1.type = NUNCHUCK; /** If the device isn't auto-detected, set the type explicatly */
+          }
+        }
+      }
+
+      // Update Lights
+      if (buffer[0] == 'U' && buffer[1] == 'L')
+      {
+        // Key Lights
+        if (buffer[2] == 'K')
+        {
+          char tmp[6] = {buffer[3], buffer[4], buffer[5], buffer[6], buffer[7], buffer[8]};
+          buttonColor = strtol(tmp, NULL, 16);
+//          Serial.println(buttonColor, HEX); // Dubug information
+        }
+
+        // Tower Lights
+        if (buffer[2] == 'T')
+        {
+          char tmp[2] = {buffer[3], buffer[4]};
+          byte towerStatus = (byte) strtol(tmp, NULL, 16);
+//          Serial.println(towerStatus, HEX); // Dubug information
+          setTowerLEDs(towerStatus);
+        }
+      }
+
+      // Animations
+      if (buffer[0] == 'A' && buffer[1] == 'N')
+      {
+        if (buffer[2] == '1')
+          towerLightAnimation();
+        if (buffer[2] == '2')
+          flatline();
+      }
+
+      // Debug Options
+      if (buffer[0] == 'D' && buffer[1] == 'B')
+      {
+        // Add later
+      }
+    }
+
+  digitalWrite2(LED_BUILTIN, nunchuckEnabled);
+  
+  if (digitalRead2f(DP22) == 0)
+    buttonPixels.setPixelColor(7, buttonColor);
+  else
+    buttonPixels.setPixelColor(7, 0);
+  if (digitalRead2f(DP23) == 0)
+    buttonPixels.setPixelColor(6, buttonColor);
+  else
+    buttonPixels.setPixelColor(6, 0);
+  if (digitalRead2f(DP24) == 0)
+    buttonPixels.setPixelColor(5, buttonColor);
+  else
+    buttonPixels.setPixelColor(5, 0);
+  if (digitalRead2f(DP25) == 0)
+    buttonPixels.setPixelColor(4, buttonColor);
+  else
+    buttonPixels.setPixelColor(4, 0);
+  if (digitalRead2f(DP26) == 0)
+    buttonPixels.setPixelColor(3, buttonColor);
+  else
+    buttonPixels.setPixelColor(3, 0);
+  if (digitalRead2f(DP27) == 0)
+    buttonPixels.setPixelColor(2, buttonColor);
+  else
+    buttonPixels.setPixelColor(2, 0);
+  if (digitalRead2f(DP28) == 0)
+    buttonPixels.setPixelColor(1, buttonColor);
+  else
+    buttonPixels.setPixelColor(1, 0);
+  if (digitalRead2f(DP29) == 0)
+    buttonPixels.setPixelColor(0, buttonColor);
+  else
+    buttonPixels.setPixelColor(0, 0);
+  buttonPixels.show();
+}
