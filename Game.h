6bcf93db2b21d1387c87be97fae15c02196d7da6
Zachary Perlmutter <zrp200@gmail.com>
2022-04-17 13:07:17 -0700

    Implemented a "bag" that ensures all pieces appear at least once before repeating pieces

--- Game.h
+++ Game.h
@@ -10,6 +10,7 @@
 #include<cmath>
 #include "Shape.h"
 #include "Utilities.h"
+#include "Bag.h"
 
 enum class GameState
 {
@@ -84,8 +85,9 @@ public:
     }
 
 private:
+    Bag bag; // for drawing pieces
     // shape logic
-    Shape *nxtShape=new Shape(),*curShape = nullptr;
+    Shape *nxtShape=new Shape(bag.draw()),*curShape = nullptr;
     void loadNewShape();
     void placeShape();
     bool moveCurShapeDown();
