587e293fc51adc7cd1b213eff0f304f320f51130
Zachary Perlmutter <zrp200@gmail.com>
2022-04-21 13:06:58 -0700

    Color now inherits from SDL_Color

--- Color.h
+++ Color.h
@@ -5,15 +5,19 @@
 #ifndef CSCCLUB_TETRIS_COLOR_H
 #define CSCCLUB_TETRIS_COLOR_H
 
-/** wrapper for color operations **/
-struct Color
-{
-    unsigned char r,g,b;
+#include<SDL2/SDL_pixels.h>
 
+/** wrapper for color operations **/
+struct Color : SDL_Color
+{
     // default color
-    constexpr Color() : Color(100, 100, 100) {}
-    constexpr Color(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b){}
-    constexpr Color(int hex) : r(hex >> 16 & 0xFF), g(hex >> 8 & 0xFF), b(hex & 0xFF){}
+    constexpr Color() : SDL_Color({100,100,100}) {}
+    constexpr Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a=0)
+        : SDL_Color({r,g,b,a}) {}
+    constexpr Color(int hex) : Color(
+            hex >> 16 & 0xFF,
+            hex >> 8 & 0xFF,
+            hex & 0xFF){}
 
     constexpr bool operator==(Color other) const { return hex() == other.hex(); }
 
